d113de2ff60eb52c9eee87395a32d29689725bcd
False <tu.marcelo2412@gmail.com>
2020-11-02 22:05:44 -0300

    codigo formatado

--- src/jokenpo/Jogador.java
+++ src/jokenpo/Jogador.java
@@ -4,77 +4,79 @@ public class Jogador {
     int vit = 0;
     int der = 0;
     int emp = 0;
-    
-    public int Vitoria(){
+
+    public int Vitoria() {
         return vit++;
     }
-    public int Derrota(){
+
+    public int Derrota() {
         return der++;
     }
-    public int Empate(){
+
+    public int Empate() {
         return emp++;
     }
 
-    public static void menu(){
+    public static void menu() {
         System.out.println("Qual o modo de jogo que o senhor deseja:");
         System.out.println("\n1 - Player vs CPU");
         System.out.println("2 - Player vs Player");
     }
 
-    public static void menuJogo(){
+    public static void menuJogo() {
         System.out.println("Escolha entre 1, 2 e 3 , sendo : ");
-    System.out.println("\nPedra = 1 ");
-    System.out.println("Papel = 2  ");
-    System.out.println("Tesoura = 3");
+        System.out.println("\nPedra = 1 ");
+        System.out.println("Papel = 2  ");
+        System.out.println("Tesoura = 3");
     }
-    
-    public static int jogo(int v1,int v2){
-    switch (v1){
-        case 1:
-            System.out.println("Você escolheu pedra");
 
-            switch (v2){
+    public static int jogo(int v1, int v2) {
+        switch (v1) {
+            case 1:
+                System.out.println("Você escolheu pedra");
 
-                case 1:
-                    System.out.println("O oponente escolheu Pedra");
-                    System.out.println("\nEmpate");
-                    return 1;
-                case 2:
-                    System.out.println("O oponente escolheu Papel");
-                    System.out.println("\nO oponente ganhou");
-                    return 2;
-                case 3:
-                    System.out.println("O oponente escolheu Tesoura");
-                    System.out.println("\nVocê ganhou");
-                    return 3;
-                default: 
-                    System.out.println ("Opção invalida do oponente");
-                    return 0;
-            } 
-        case 2:
+                switch (v2) {
 
-            System.out.println("Você escolheu Papel");
-
-            switch(v2) {
-                case 1: 
-                    System.out.println("O oponente escolheu Pedra");
-                    System.out.println("\nVocê ganhou");
-                    return 3;
-                case 2:
-                    System.out.println("O oponente escolheu Papel");
-                    System.out.println("\nEmpatou");
-                    return 1;
-                case 3:
-                    System.out.println("O oponente escolheu Tesoura");
-                    System.out.println("\nO oponente ganhou");
-                    return 2;
-                default: 
-                        System.out.println ("Opção invalida do oponente");
+                    case 1:
+                        System.out.println("O oponente escolheu Pedra");
+                        System.out.println("\nEmpate");
+                        return 1;
+                    case 2:
+                        System.out.println("O oponente escolheu Papel");
+                        System.out.println("\nO oponente ganhou");
+                        return 2;
+                    case 3:
+                        System.out.println("O oponente escolheu Tesoura");
+                        System.out.println("\nVocê ganhou");
+                        return 3;
+                    default:
+                        System.out.println("Opção invalida do oponente");
                         return 0;
-            } 
-        case 3:
-                        System.out.println("Você escolheu Tesoura");
-            switch(v2) {
+                }
+            case 2:
+
+                System.out.println("Você escolheu Papel");
+
+                switch (v2) {
+                    case 1:
+                        System.out.println("O oponente escolheu Pedra");
+                        System.out.println("\nVocê ganhou");
+                        return 3;
+                    case 2:
+                        System.out.println("O oponente escolheu Papel");
+                        System.out.println("\nEmpatou");
+                        return 1;
+                    case 3:
+                        System.out.println("O oponente escolheu Tesoura");
+                        System.out.println("\nO oponente ganhou");
+                        return 2;
+                    default:
+                        System.out.println("Opção invalida do oponente");
+                        return 0;
+                }
+            case 3:
+                System.out.println("Você escolheu Tesoura");
+                switch (v2) {
                     case 1:
                         System.out.println("O oponente escolheu Pedra");
                         System.out.println("\nO oponente ganhou");
@@ -87,19 +89,22 @@ public class Jogador {
                         System.out.println("O oponente escolheu Tesoura");
                         System.out.println("\nEmpate");
                         return 1;
-                    default: 
-                        System.out.println ("Opção invalida do oponente");
-                                                     return 0;
-            }
-        default: 
-            System.out.println ("Opção invalida");
-                            return 0;
-}}
-    public static void menuSair(){
-
-    System.out.println("\nDigite 1 para sair...");
+                    default:
+                        System.out.println("Opção invalida do oponente");
+                        return 0;
+                }
+            default:
+                System.out.println("Opção invalida");
+                return 0;
+        }
     }
-    public void res(){
-        System.out.println("Vitórias: " + vit + "\nDerrotas: "+ der + "\nEmpates: "+ emp);
+
+    public static void menuSair() {
+
+        System.out.println("\nDigite 1 para sair...");
+    }
+
+    public void res() {
+        System.out.println("Vitórias: " + vit + "\nDerrotas: " + der + "\nEmpates: " + emp);
     }
 }
--- src/jokenpo/Jokenpo.java
+++ src/jokenpo/Jokenpo.java
@@ -10,185 +10,182 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Jokenpo extends Jogador {
-        public static void main (String args []) throws IOException{
-		
-		Scanner sc = new Scanner(System.in);
-                Random aleatorio = new Random();               
-		int v1,v2,gamemode,resultado;
-                int sair;
-                Jogador player = new Jogador();
+    public static void main(String args[]) throws IOException {
 
-                 
-                menu();
+        Scanner sc = new Scanner(System.in);
+        Random aleatorio = new Random();
+        int v1, v2, gamemode, resultado;
+        int sair;
+        Jogador player = new Jogador();
+
+        menu();
+
+        gamemode = sc.nextInt();
+
+        switch (gamemode) {
+
+            case 1:
+
+                do {
+                    do {
+                        menuJogo();
+
+                        System.out.println("\nSua jogada:");
+                        v1 = sc.nextInt();
+                        if (v1 != 1 && v1 != 2 && v1 != 3) {
+                            System.out.println("\nJogada Invalida...");
+                        }
+
+                    } while (v1 != 1 && v1 != 2 && v1 != 3);
+
+                    v2 = aleatorio.nextInt(3) + 1;
+                    resultado = jogo(v1, v2);
+                    if (resultado == 1) {
+                        player.Empate();
+                    }
+                    if (resultado == 2) {
+                        player.Derrota();
+                    }
+                    if (resultado == 3) {
+                        player.Vitoria();
+                    }
+                    player.res();
+                    menuSair();
+
+                    sair = sc.nextInt();
+                } while (sair != 1);
+                break;
+
+            case 2:
+                System.out.println("1 - Criar sua sala");
+                System.out.println("2 - Entrar em uma sala");
 
                 gamemode = sc.nextInt();
-               
-                switch(gamemode){
-                    
+
+                switch (gamemode) {
                     case 1:
-                            
-                        do{
-                            do{
-                                menuJogo();
-                            
-                                System.out.println("\nSua jogada:");
-                                v1 = sc.nextInt();
-                                if(v1 != 1 && v1 != 2 && v1 != 3){
-                                    System.out.println("\nJogada Invalida...");
+                        try {
+                            ServerSocket server = new ServerSocket(2323);
+
+                            System.out.println("Seu IP é:" + InetAddress.getLocalHost().getHostAddress());
+                            System.out.println("Aguardando outro jogador.");
+
+                            Socket cliente = server.accept();
+
+                            DataOutputStream saida = new DataOutputStream(cliente.getOutputStream());
+                            DataInputStream entrada = new DataInputStream(cliente.getInputStream());
+
+                            System.out.println("Jogador Conectado");
+
+                            int joserver, joclient;
+
+                            do {
+
+                                System.out.println("faça sua jogada");
+                                do {
+                                    menuJogo();
+                                    joserver = sc.nextInt();
+
+                                    if (joserver != 1 && joserver != 2 && joserver != 3) {
+
+                                        System.out.println("Jogada Invalida... \nTente Novamente");
+
+                                    }
+
+                                } while (joserver != 1 && joserver != 2 && joserver != 3);
+                                saida.flush();
+                                saida.write(joserver);
+
+                                System.out.println("Esperando jogada do oponente...");
+                                joclient = (int) entrada.read();
+                                resultado = jogo(joserver, joclient);
+                                if (resultado == 1) {
+                                    player.Empate();
+                                }
+                                if (resultado == 2) {
+                                    player.Derrota();
+                                }
+                                if (resultado == 3) {
+                                    player.Vitoria();
                                 }
 
-                            }while(v1 != 1 && v1 != 2 && v1 != 3);
+                                player.res();
+                                menuSair();
 
-                            v2 = aleatorio.nextInt(3)+1;
-                            resultado = jogo(v1,v2);
-                            if(resultado == 1){
-                                player.Empate();
-                            }
-                            if(resultado == 2){
-                                player.Derrota();
-                            }
-                            if(resultado == 3){
-                                player.Vitoria();
-                            }
-                            player.res();
-                            menuSair();
+                                sair = sc.nextInt();
 
-                            sair= sc.nextInt();
-                        }while(sair!=1);
+                            } while (sair != 1);
+
+                            server.close();
+
                             break;
 
+                        } catch (Exception e) {
+                            System.out.println(e);
+                            break;
+                        }
+
                     case 2:
-                            System.out.println("1 - Criar sua sala");
-                            System.out.println("2 - Entrar em uma sala");
+                        try {
+                            int jogada1, jogada2;
 
-                            gamemode = sc.nextInt();
-                            
-                            switch(gamemode){
-                                case 1:
-                                    try{
-                                    ServerSocket server = new ServerSocket(2323);
-                                    
-                                    System.out.println("Seu IP é:"+ InetAddress.getLocalHost().getHostAddress());
-                                    System.out.println("Aguardando outro jogador.");
-                                    
-                                    Socket cliente = server.accept();
-                                    
-                                    DataOutputStream saida = new DataOutputStream(cliente.getOutputStream());
-                                    DataInputStream entrada = new DataInputStream(cliente.getInputStream());
-                                    
-                                    System.out.println("Jogador Conectado");
-                                    
-                                    int joserver, joclient;
+                            String IP;
 
-                                    do{
+                            System.out.println("Digite o IP da sala:");
 
-                                        System.out.println("faça sua jogada");
-                                        do{
-                                            menuJogo();
-                                            joserver = sc.nextInt();
-                                           
-                                            if(joserver != 1 && joserver != 2 && joserver != 3){
+                            IP = sc.next();
 
-                                            System.out.println("Jogada Invalida... \nTente Novamente");
+                            Socket conexao = new Socket(IP, 2323);
+                            DataOutputStream out = new DataOutputStream(conexao.getOutputStream());
+                            DataInputStream in = new DataInputStream(conexao.getInputStream());
 
-                                            }
+                            System.out.println("Partida encontrada");
 
-                                        }while(joserver != 1 && joserver != 2 && joserver != 3);
-                                        saida.flush();
-                                        saida.write(joserver);
-                                        
-                                        
-                                        System.out.println("Esperando jogada do oponente...");
-                                        joclient = (int)entrada.read();
-                                        resultado = jogo(joserver,joclient);
-                                        if(resultado == 1){
-                                            player.Empate();
-                                        }
-                                        if(resultado == 2){
-                                            player.Derrota();
-                                        }
-                                        if(resultado == 3){
-                                            player.Vitoria();
-                                        }
-                                        
-                                        player.res();                                            
-                                        menuSair();
-                                        
-                                        sair = sc.nextInt();
-                                        
-                                    }while(sair!=1);
+                            do {
 
-                                    server.close();
-                                    
-                                    break;
+                                System.out.println("Digite sua jogada...");
 
-                                }catch(Exception e){
-                                    System.out.println(e);
-                                    break;
+                                do {
+                                    menuJogo();
+                                    jogada1 = sc.nextInt();
+
+                                    if (jogada1 != 1 && jogada1 != 2 && jogada1 != 3) {
+
+                                        System.out.println("Jogada Invalida... \nTente Novamente");
+
+                                    }
+                                } while (jogada1 != 1 && jogada1 != 2 && jogada1 != 3);
+
+                                out.write(jogada1);
+                                jogada2 = (int) in.read();
+
+                                resultado = jogo(jogada1, jogada2);
+                                if (resultado == 1) {
+                                    player.Empate();
                                 }
+                                if (resultado == 2) {
+                                    player.Derrota();
+                                }
+                                if (resultado == 3) {
+                                    player.Vitoria();
+                                }
+                                player.res();
+                                menuSair();
 
-                                case 2:
-                                    try{
-                                    int jogada1,jogada2;
-                                    
-                                    String IP;
+                                sair = sc.nextInt();
 
-                                    System.out.println("Digite o IP da sala:");
-                                    
-                                    IP = sc.next();
-                                    
-                                    Socket conexao = new Socket(IP,2323);
-                                    DataOutputStream out = new DataOutputStream(conexao.getOutputStream());
-                                    DataInputStream in = new DataInputStream(conexao.getInputStream());
-                                    
-                                    System.out.println("Partida encontrada");
-                                    
-                                    do{
-                                    
-                                        System.out.println("Digite sua jogada...");
-                                        
-                                        do{
-                                            menuJogo();
-                                            jogada1 = sc.nextInt();   
+                            } while (sair != 1);
 
-                                            if(jogada1 != 1 && jogada1 != 2 && jogada1 != 3){
-                                                    
-                                                    System.out.println("Jogada Invalida... \nTente Novamente");
-                                                    
-                                                }
-                                        }while(jogada1 != 1 && jogada1 != 2 && jogada1 != 3);
-                                        
-                                        out.write(jogada1);
-                                        jogada2 = (int)in.read();
-                                        
-                                        resultado = jogo(jogada1,jogada2);
-                                        if(resultado == 1){
-                                            player.Empate();
-                                        }
-                                        if(resultado == 2){
-                                            player.Derrota();
-                                        }
-                                        if(resultado == 3){
-                                            player.Vitoria();
-                                        }
-                                        player.res();
-                                        menuSair();
-                                        
-                                        sair = sc.nextInt();
+                            conexao.close();
 
-                                    }while(sair!=1);
-                                 
-                                    conexao.close();
-            
-                                        }catch(Exception e){
-                                            System.out.println(e);
-                                            break;
-                                        }
+                        } catch (Exception e) {
+                            System.out.println(e);
+                            break;
+                        }
+                }
+
+                sc.close();
+
         }
-                                        
-     sc.close();
-                                    
-                                    
-            }
-        }        
-	}
+    }
+}
